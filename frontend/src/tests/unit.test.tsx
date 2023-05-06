5fd577b41f866dfb4e83f71aaeb3caefba082ff7
curruy123 <kyle_sohn@brown.edu>
2023-05-06 15:03:28 -0400

    w unit test

--- frontend/src/tests/unit.test.tsx
+++ frontend/src/tests/unit.test.tsx
@@ -1,74 +1,112 @@
-import { render, screen } from "@testing-library/react";
-import { ProductPriceStats } from "../components/ProductPriceStats";
+import { getPrices, getProduct } from "../data/getPrices";
+import {handleAddToWishlist3, handleRemoveFromWishlist3} from "../data/getWishlist"
+import { IProduct } from "../data/dataTypes";
+import { userSignedIn } from "../firebase";
 
+describe("getPrices", () => {
+  it("should return prices for a valid product key", () => {
+    const prices = getPrices("Gazelle-bold-shoes");
+    expect(prices).toEqual([
+      { vendor: "Vendor 1", price: 45 },
+      { vendor: "Vendor 2", price: 45.67 },
+      { vendor: "Vendor 3", price: 46 },
+    ]);
+  });
 
-describe("ProductPriceStats", () => {
-  const sku = "example_sku";
+  it("should return an empty array for an invalid product key", () => {
+    const prices = getPrices("invalid-key");
+    expect(prices).toEqual([]);
+  });
+});
 
-  describe("when price stats list is defined", () => {
-    beforeEach(() => {
-      const mapProductPrice = new Map();
-      const priceStats = [
-        {
-          platformName: "Example Platform",
-          minPriceUsd: 40,
-          avgPriceUsd: 20,
-          lastPriceUsd: 30,
-        },
-      ];
-      mapProductPrice.set(sku, priceStats);
-
-      render(<ProductPriceStats sku={sku} mapProductPrice={mapProductPrice} />);
-    });
-
-    it("displays price values", () => {
-      const minPrice = screen.getByText("40");
-      const avgPrice = screen.getByText("20");
-      const lastPrice = screen.getByText("30");
-      expect(minPrice).toBeInTheDocument();
-      expect(avgPrice).toBeInTheDocument();
-      expect(lastPrice).toBeInTheDocument();
+describe("getProduct", () => {
+  it("should return a product for a valid product key", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(product).toEqual({
+      key: "Gazelle-bold-shoes",
+      value: "Nike gazelle bold shoes",
+      image:
+        "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/c69922d42fc84de28e9baf5400edaf14_9366/Gazelle_Bold_Shoes_Black_HQ4408_02_standard.jpg",
+      prices: [
+        { vendor: "Vendor 1", price: 45 },
+        { vendor: "Vendor 2", price: 45.67 },
+        { vendor: "Vendor 3", price: 46 },
+      ],
     });
   });
 
-  describe("when filtering price stats by selected platforms", () => {
-    beforeEach(() => {
-      const mapProductPrice = new Map();
-      const priceStats = [
-        {
-          platformName: "Platform 1",
-          minPriceUsd: 10,
-          avgPriceUsd: 20,
-          lastPriceUsd: 30,
-        },
-        {
-          platformName: "Platform 2",
-          minPriceUsd: 15,
-          avgPriceUsd: 25,
-          lastPriceUsd: 35,
-        },
-      ];
-      mapProductPrice.set(sku, priceStats);
+  it("should return undefined for an invalid product key", () => {
+    const product = getProduct("invalid-product-key");
+    expect(product).toBeUndefined();
+  });
 
-      const pageState = {
-        platforms: [
-          { name: "Platform 1", selected: true },
-          { name: "Platform 2", selected: false },
-        ],
-      };
+  it("should return the first product for a duplicate product key", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    const duplicateProduct = getProduct("Gazelle-bold-shoes");
+    expect(duplicateProduct).toEqual(product);
+  });
 
-      render(
-        <ProductPriceStats
-          sku={sku}
-          mapProductPrice={mapProductPrice}
-          pageState={pageState}
-        />
-      );
-    });
+  it("should return a product with a non-empty prices array", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(product.prices.length).toBeGreaterThan(0);
+  });
 
-    it("displays only selected platform", () => {
-      const platformName2 = screen.queryByText("Platform 2");
-      expect(platformName2).toBeNull();
+  it("should return a product with prices that are numbers", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    for (let price of product.prices) {
+      expect(typeof price.price).toBe("number");
+    }
+  });
+
+  it("should return a product with prices that are greater than or equal to zero", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    for (let price of product.prices) {
+      expect(price.price).toBeGreaterThanOrEqual(0);
+    }
+  });
+
+  it("should return a product with prices that are not NaN", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    for (let price of product.prices) {
+      expect(isNaN(price.price)).toBe(false);
+    }
+  });
+
+  it("should return a product with a key that is a non-empty string", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(typeof product.key).toBe("string");
+    expect(product.key.length).toBeGreaterThan(0);
+  });
+
+  it("should return a product with a value that is a non-empty string", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(typeof product.value).toBe("string");
+    expect(product.value.length).toBeGreaterThan(0);
+  });
+
+  it("should return a product with an image that is a non-empty string", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(typeof product.image).toBe("string");
+    expect(product.image.length).toBeGreaterThan(0);
+  });
+
+});
+
+
+describe("getProduct", () => {
+  it("should return a product for a valid product key", () => {
+    const product = getProduct("Gazelle-bold-shoes");
+    expect(product).toEqual({
+      key: "Gazelle-bold-shoes",
+      value: "Nike gazelle bold shoes",
+      image:
+        "https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/c69922d42fc84de28e9baf5400edaf14_9366/Gazelle_Bold_Shoes_Black_HQ4408_02_standard.jpg",
+      prices: [
+        { vendor: "Vendor 1", price: 45 },
+        { vendor: "Vendor 2", price: 45.67 },
+        { vendor: "Vendor 3", price: 46 },
+      ],
     });
   });
 });
+
