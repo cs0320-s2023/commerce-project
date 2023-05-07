5793ab48a63b5bff1763ab113142c36c1084e22e
curruy123 <kyle_sohn@brown.edu>
2023-05-07 03:10:26 -0400

    added dom tests

--- frontend/src/tests/dom.test.tsx
+++ frontend/src/tests/dom.test.tsx
@@ -2,23 +2,27 @@ import "@testing-library/jest-dom";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
 import userEvent from "@testing-library/user-event";
-import {jest} from '@jest/globals'
+import { jest } from "@jest/globals";
 import "@testing-library/dom";
 import "@testing-library/jest-dom";
-
-import App from "../App"
-import {ProductDescription, NewSearchBar, SearchResults} from  "../components"
-
-beforeEach(() => {render(<App />);});
+import { Wishlist } from "../components/Wishlist";
+import { GoogleSignIn} from "../components";
 
 
-  test('renders input and button elements', () => {
-    render(<NewSearchBar />);
-    const inputElement = screen.getByRole('input-search-box');
-    const buttonElement = screen.getByRole('button', { name: /search/i });
-    expect(inputElement).toBeInTheDocument();
-    expect(buttonElement).toBeInTheDocument();
-  })
+import App from "../App";
+import { ProductDescription, NewSearchBar, SearchResults } from "../components";
+
+beforeEach(() => {
+  render(<App />);
+});
+
+test("renders input and button elements", () => {
+  render(<NewSearchBar />);
+  const inputElement = screen.getByRole("input-search-box");
+  const buttonElement = screen.getByRole("button", { name: /search/i });
+  expect(inputElement).toBeInTheDocument();
+  expect(buttonElement).toBeInTheDocument();
+});
 
 test("renders components", () => {
   // expect(screen.getByRole("input-search-box")).toBeInTheDocument();
@@ -27,33 +31,56 @@ test("renders components", () => {
   expect(screen.getByRole("app")).toBeInTheDocument();
 });
 
-  test("input element accepts user input", () => {
-    render(<NewSearchBar />);
-    const inputElement = screen.getByRole("input-search-box");
-    fireEvent.change(inputElement, { target: { value: "Adidas" } });
-    expect(inputElement).toHaveValue("Adidas");
-  });
+test("input element accepts user input", () => {
+  render(<NewSearchBar />);
+  const inputElement = screen.getByRole("input-search-box");
+  fireEvent.change(inputElement, { target: { value: "Adidas" } });
+  expect(inputElement).toHaveValue("Adidas");
+});
 
-  test("search button is enabled when input is not empty", () => {
-    render(<NewSearchBar />);
-    const searchButton = screen.getByRole("button", { name: "Search" });
-    const input = screen.getByRole("input-search-box");
-    fireEvent.change(input, { target: { value: "sneakers" } });
-    expect(searchButton).toBeEnabled();
-  });
+test("search button is enabled when input is not empty", () => {
+  render(<NewSearchBar />);
+  const searchButton = screen.getByRole("button", { name: "Search" });
+  const input = screen.getByRole("input-search-box");
+  fireEvent.change(input, { target: { value: "sneakers" } });
+  expect(searchButton).toBeEnabled();
+});
 
-  test("search button is enabled when input is not empty", () => {
-    render(<NewSearchBar />);
-    const searchButton = screen.getByRole("button", { name: "Search" });
-    const input = screen.getByRole("input-search-box");
-    fireEvent.change(input, { target: { value: "sneakers" } });
-    expect(searchButton).toBeEnabled();
-  });
+test("search button is enabled when input is not empty", () => {
+  render(<NewSearchBar />);
+  const searchButton = screen.getByRole("button", { name: "Search" });
+  const input = screen.getByRole("input-search-box");
+  fireEvent.change(input, { target: { value: "sneakers" } });
+  expect(searchButton).toBeEnabled();
+});
 
-  test("search button is disabled when input is empty", () => {
-    render(<NewSearchBar />);
-    const searchButton = screen.getByRole("button", { name: "Search" });
-    expect(searchButton).toBeEnabled();
-  });
+test("search button is disabled when input is empty", () => {
+  render(<NewSearchBar />);
+  const searchButton = screen.getByRole("button", { name: "Search" });
+  expect(searchButton).toBeEnabled();
+});
 
- 
+import React from "react";
+
+
+test("renders sign in button when user is not signed in", () => {
+  const { getByLabelText, getByText } = render(<GoogleSignIn />);
+  const signInButton = getByLabelText("Press Enter to Sign In With Google");
+  const signInText = getByText("Sign in!");
+  const googleIcon = getByLabelText("Google logo");
+
+  expect(signInButton).toBeInTheDocument();
+  expect(signInText).toBeInTheDocument();
+  expect(googleIcon).toBeInTheDocument();
+});
+
+test("renders sign out button when user is signed in", () => {
+  const { getByLabelText, getByText } = render(<GoogleSignIn />);
+  const signOutButton = getByLabelText("Press Enter to Sign Out");
+  const signOutText = getByText("Sign Out!");
+  const googleIcon = getByLabelText("Google logo");
+
+  expect(signOutButton).toBeInTheDocument();
+  expect(signOutText).toBeInTheDocument();
+  expect(googleIcon).toBeInTheDocument();
+});
--- frontend/src/tests/unit.test.tsx
+++ frontend/src/tests/unit.test.tsx
@@ -1,13 +1,14 @@
 import { getPrices, getProduct } from "../data/getPrices";
-import {handleAddToWishlist3, handleRemoveFromWishlist3} from "../data/getWishlist"
+import {
+  handleAddToWishlist3,
+  handleRemoveFromWishlist3,
+} from "../data/getWishlist";
 import { IProduct } from "../data/dataTypes";
 import { userSignedIn } from "../firebase";
 import { getPriceStats } from "../data/getPriceStats";
 import { mockPriceStats } from "../../../mockdata/priceStats";
 import { getProductList } from "../data/getProductList";
-import { secretAPIkey } from "../data/apikey";
-
-
+import { secretAPIKey } from "../data/apikey";
 
 describe("getPrices", () => {
   it("should return prices for a valid product key", () => {
@@ -95,9 +96,7 @@ describe("getProduct", () => {
     expect(typeof product.image).toBe("string");
     expect(product.image.length).toBeGreaterThan(0);
   });
-
 });
-
 
 describe("getProduct", () => {
   it("should return a product for a valid product key", () => {
@@ -118,105 +117,108 @@ describe("getProduct", () => {
 
 // unit testing getPriceStats
 
-
-describe('getPriceStats', () => {
+describe("getPriceStats", () => {
   const mockDispatch = jest.fn();
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
-  it('should dispatch priceStatsSuccess action with mock data when mockingMode is true', async () => {
-
-    const mockSku = 'abc123';
+  it("should dispatch priceStatsSuccess action with mock data when mockingMode is true", async () => {
+    const mockSku = "abc123";
 
     await getPriceStats(mockSku, mockDispatch);
 
     expect(mockDispatch).toHaveBeenCalledWith({
-      type: 'priceStatsSuccess',
+      type: "priceStatsSuccess",
       payload: mockPriceStats.data,
       sku: mockSku,
     });
   });
 
   // testing getProductList
-// Mock the dispatch function and the response data for successful and error scenarios
+  // Mock the dispatch function and the response data for successful and error scenarios
 
-const mockSuccessResponse = { data: [{ id: 1, name: 'Nike Air Force 1' }] };
-const mockErrorResponse = { message: 'Error occurred' };
+  const mockSuccessResponse = { data: [{ id: 1, name: "Nike Air Force 1" }] };
+  const mockErrorResponse = { message: "Error occurred" };
 
-// Mock the fetch function with resolved promises to simulate success and failure scenarios
-jest.mock('node-fetch', () => ({
-  __esModule: true,
-  default: jest.fn().mockImplementation((url) => {
-    if (url.includes('error')) {
-      return Promise.reject(mockErrorResponse);
-    } else {
-      return Promise.resolve({
-        json: () => Promise.resolve(mockSuccessResponse),
-      });
-    }
-  }),
-}));
+  // Mock the fetch function with resolved promises to simulate success and failure scenarios
+  jest.mock("node-fetch", () => ({
+    __esModule: true,
+    default: jest.fn().mockImplementation((url) => {
+      if (url.includes("error")) {
+        return Promise.reject(mockErrorResponse);
+      } else {
+        return Promise.resolve({
+          json: () => Promise.resolve(mockSuccessResponse),
+        });
+      }
+    }),
+  }));
 
-describe('getProductList', () => {
-  const backendURL = "https://sneakers-real-time-pricing.p.rapidapi.com";
-    const options = {  
-      method: 'GET',
+  describe("getProductList", () => {
+    const backendURL = "https://sneakers-real-time-pricing.p.rapidapi.com";
+    const options = {
+      method: "GET",
       headers: {
-        'X-RapidAPI-Key': secretAPIkey,
-      }, };
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+        "X-RapidAPI-Key": secretAPIKey,
+      },
+    };
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
 
-  it('should dispatch searchSuccess action with correct payload on successful response', async () => {
-    // Arrange
-    const searchText = 'nike';
-    const expectedAction = { type: 'searchSuccess', payload: mockSuccessResponse.data };
+    it("should dispatch searchSuccess action with correct payload on successful response", async () => {
+      // Arrange
+      const searchText = "nike";
+      const expectedAction = {
+        type: "searchSuccess",
+        payload: mockSuccessResponse.data,
+      };
 
-    // Act
-    await getProductList(searchText, mockDispatch);
-    
-    
-    // Assert
-    // expect(fetch).toHaveBeenCalledTimes(1);
-    // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
-  });
+      // Act
+      await getProductList(searchText, mockDispatch);
 
-  it('should dispatch searchFailure action with correct payload on server error response', async () => {
-    // Arrange
-    const searchText = 'error';
-    const expectedAction = { type: 'searchFailure', payload: mockErrorResponse.message };
+      // Assert
+      // expect(fetch).toHaveBeenCalledTimes(1);
+      // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+    });
 
-    // Act
-    await getProductList(searchText, mockDispatch);
+    it("should dispatch searchFailure action with correct payload on server error response", async () => {
+      // Arrange
+      const searchText = "error";
+      const expectedAction = {
+        type: "searchFailure",
+        payload: mockErrorResponse.message,
+      };
 
-    // Assert
-    // expect(fetch).toHaveBeenCalledTimes(1);
-    // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
-    // expect(mockDispatch).toHave(expectedAction);
-  });
+      // Act
+      await getProductList(searchText, mockDispatch);
 
-  it('should dispatch searchFailure action with generic error message on network error', async () => {
-    // Arrange
-    const searchText = 'network error';
-    const expectedAction = { type: 'searchFailure', payload: 'Error while trying to contact server' };
+      // Assert
+      // expect(fetch).toHaveBeenCalledTimes(1);
+      // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      // expect(mockDispatch).toHave(expectedAction);
+    });
 
-    // Act
-    await getProductList(searchText, mockDispatch);
+    it("should dispatch searchFailure action with generic error message on network error", async () => {
+      // Arrange
+      const searchText = "network error";
+      const expectedAction = {
+        type: "searchFailure",
+        payload: "Error while trying to contact server",
+      };
 
-    // Assert
-    // expect(fetch).toHaveBeenCalledTimes(1);
-    // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
-    expect(mockDispatch).toHaveBeenCalledTimes(1);
-    // expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+      // Act
+      await getProductList(searchText, mockDispatch);
+
+      // Assert
+      // expect(fetch).toHaveBeenCalledTimes(1);
+      // expect(fetch).toHaveBeenCalledWith(`${backendURL}/sneakers?extended=true&name=${searchText}`, options);
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      // expect(mockDispatch).toHaveBeenCalledWith(expectedAction);
+    });
   });
 });
-
-
-
-});
-
