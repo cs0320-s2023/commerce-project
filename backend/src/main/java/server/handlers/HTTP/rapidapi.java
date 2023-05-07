98e9db86f3391df269a6b047a71408280d12ea17
smmbu2023 <113218975+smmbu2023@users.noreply.github.com>
2023-05-07 08:03:51 -0400

    Update rapidapi.java

--- backend/src/main/java/server/handlers/HTTP/rapidapi.java
+++ backend/src/main/java/server/handlers/HTTP/rapidapi.java
@@ -1,5 +1,5 @@
 package server.handlers.HTTP;
 
 public class rapidapi {
-    public static String apiKey = "1fde064a92msha5dc91d2825fe04p137195jsn2f5b6deb60bc";
+    public static String apiKey = "Provide your key here";
 }
